8ff6d6ef6163da118abc770e3852ee6efdb1e1e8
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-14 21:43:38 -0300

    Comentários de Código

---
+++ fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// Comentário de uma única linha
+console.log("Linha 1")
+
+/* 
+    Comentário de   
+    múltiplas linhas
+*/
+console.log('Linhas 2')
+
+/*
+* 
+* Comentário de 
+* múltiplas linhas
+*
+*/
