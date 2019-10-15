1d46e90acbeb39894feff8a9e3ac7e98ec26723f
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-14 22:00:46 -0300

    Tipagem Fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,17 @@
+// Linguagem de tipagem fraca
+
+let qualquer = 'Legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.123
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genéricos e siglas
+let valor = 3
+let numero = 4
+let pqp = false // Produto Químico Perigoso... ushsuhsu
+
+// Prefira usar variáveis claras à criar comentários de código desnecessários
