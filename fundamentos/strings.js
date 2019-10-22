fffec3276e2d0724eb24ffc5189d832e9839a184
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-21 22:23:03 -0300

    Introdução stringss

---
+++ fundamentos/strings.js
@@ -0,0 +1,15 @@
+const teste = "POG"
+
+console.log(teste.charAt(2))
+console.log(teste.charCodeAt(2))
+console.log(teste.indexOf('O'))
+
+console.log(teste.substring(1))
+console.log(teste.substring(0,2))
+
+console.log('teste'.concat(teste).concat("!"))
+console.log(teste.replace('G', 'T')) 
+
+console.log('A,B,C,D'.split(','))
+console.log('A,B,C,D'.split(/,/))
+ 
