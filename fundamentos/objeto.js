46dfbf50891e20d06522e377bad8d15b96c53558
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-23 18:33:36 -0300

    Objetos

---
+++ fundamentos/objeto.js
@@ -0,0 +1,23 @@
+const prod1 = {}
+
+prod1.nome = 'Celular'
+prod1.preco = 4993.20
+// É posssivel criar atributos com espaços
+prod1['Desconto Final de Semana'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+
+    nome: 'Camiseta',
+    preco: 60,
+    obj1: {
+        teste: 00,
+        obj2:{
+            teste2: 22
+        }
+    }
+
+}
+
+console.log(prod2)
